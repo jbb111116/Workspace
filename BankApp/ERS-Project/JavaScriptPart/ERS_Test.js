3c67c146652ad4277b92f1872d2724251bc62771
jbb111116 <jon.bed@hotmail.com>
2018-10-29 12:33:33 -0500

    JavaScript file to send a request to the database

---
+++ BankApp/ERS-Project/JavaScriptPart/ERS_Test.js
@@ -0,0 +1,24 @@
+const getData = async function() {
+    /**
+     * Using Fetch
+     */
+    const prefixUrl = 'localhost:8080/';
+    let searchUsername = document.getElementById('search-input-username').value;
+    let searchPass = document.getElementById('search-input-pass').value;
+    let url = prefixUrl + searchUsername + '/' +searchPass;
+
+
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+
+
+    for(let url of data.povCharacters){
+        const charResponse = await fetch(url);
+        const charData = await charResponse.json();
+        const li = document.createElement('li');
+        li.innerText = charData.name;
+        document.getElementById('character-list').appendChild(li);
+    }
+}
+getData();
